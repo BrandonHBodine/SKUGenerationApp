9b2f8558e6e083c5e217c3ab4eb32063ce1109cd
BrandonHBodine <brandonhbodine@gmail.com>
2014-10-20 10:28:32 -0600

    The pedestal.js file needs to be redone to fit with the rest of the generators, mainly the standard size function

--- App/js/cubeTable.js
+++ App/js/cubeTable.js
@@ -1,14 +1,14 @@
 var cubeTable = {
-	"standardWidthsDepths":[11.5, 15, 18, 20, 23],
-	"standardHeights" : [12, 18, 24, 30, 36, 42],
-	"generator": function(){		
+	"standardWidthsDepths": [18, 23],
+	"standardHeights" : [18, 23],
+	"generator": function () {	
 
 		for (var m = 0; m < melamines.length; m++){
 			
 			calcVersion = pedestal.melaminePriceCalc;
 			starter = "CUBE";
 			color = melamines[m];
-			pedestal.tableBuilder();
+			cubeTable.tableBuilder();
 			
 		} // Melamines Loop End
 		
@@ -17,7 +17,7 @@ var cubeTable = {
 			calcVersion = pedestal.laminatePriceCalc;
 			starter = "CUBE";
 			color = laminates[l];
-			pedestal.tableBuilder();
+			cubeTable.tableBuilder();
 		} // Laminates Loop End
 		
 		for (var b = 0; b < aluminums.length; b++){
@@ -25,7 +25,7 @@ var cubeTable = {
 			calcVersion = pedestal.aluminumPriceCalc;
 			starter = "CUBE";
 			color = aluminums[b];
-			pedestal.tableBuilder();
+			cubeTable.tableBuilder();
 	
 		} // Brushed Aluminum Loop End		
 	
@@ -34,7 +34,7 @@ var cubeTable = {
 			calcVersion = pedestal.veenerPriceCalc;
 			starter = "CUBE";
 			color = veneers[v];
-			pedestal.tableBuilder();
+			cubeTable.tableBuilder();
 
 		} // Venner Loop End
 		// Event Handler add for the new elements
@@ -42,4 +42,48 @@ var cubeTable = {
 			$( this ).toggleClass("success");
 		});
 	}, // Generater End
+	
+	"tableBuilder": function(){
+		
+		for (var i = 0; i < pedestal.standardWidthsAndDepths.length; i++) {
+				
+				/* Iterate through the heights for each for the width depth */
+				width = cubeTable.standardWidthsAndDepths[i];
+				depth = cubeTable.standardWidthsAndDepths[i];
+
+				for (var h = 0; h < cubeTable.standardHeights.length; h++) {
+					// Iterate through the hieghts
+					
+					for (var top = 0; top < pedestal.topOptions.length; top++) {
+						// Top Options Interation
+						
+						for (var bottom = 0; bottom < pedestal.bottomOptions.length; bottom++) {
+							// Bottom Options Iterations
+		
+							
+								/* Calculate the price of all each hieght widht depth combonations */
+								var x = calcVersion(width, depth, pedestal.standardHeights[h]),
+												topAddOnPrice = pedestal[pedestal.topOptions[top]].price(width),
+												bottomAddOnPrice = pedestal[pedestal.bottomOptions[bottom]].price(width),
+												skuSuffix = pedestal[pedestal.topOptions[top]].suffix,
+												skuPrefix = pedestal[pedestal.bottomOptions[bottom]].prefix,
+												tbody = $("#price-table"),
+												row = $("<tr>").addClass("row-" + h),
+												sku = $("<td>").addClass("sku"),
+												size = $("<td>").addClass("size"),
+												price = $("<td>").addClass("price");						
+
+								sku.text(starter + skuSuffix + "-" + (Math.ceil(width)).toString() + (Math.ceil(depth)).toString() + color + "-" + (cubeTable.standardHeights[h]).toString() + skuPrefix);
+								price.text(x + topAddOnPrice + bottomAddOnPrice);
+								size.text( width + " x " + depth + " x " + cubeTable.standardHeights[h]);
+								row.append(sku);
+								row.append(size);
+								row.append(price);
+								tbody.append(row);
+								
+						} // Bottom Options End 
+					} // Top Options End 
+				} // Inside For Loop End
+			} // Outisde For Loop End	
+	}, // tableBuilder End
 }
