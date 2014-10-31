cae4cce80f115b1b2e3105c081b01f8f6eeecbc7
BrandonHBodine <brandonhbodine@gmail.com>
2014-10-31 13:57:37 -0600

    Export works well and the a button has been place on the page.Will be adding more features like doing percent off and product descriptions

--- App/js/main.js
+++ App/js/main.js
@@ -155,39 +155,40 @@ $( document ).ready(function() {
 				console.log(finishChoice);
 	})
 	
-	/*Trying the Export to csv for the table*/
-			function exportTableToCSV($table, filename) {
+	/* Export to csv for the table found on stack overflow at http://stackoverflow.com/questions/16078544/export-to-csv-using-jquery-and-html */
+	
+	function exportTableToCSV($table, filename) {
+		
+    var $rows = $table.find('tr:has(td)'),
 
-        var $rows = $table.find('tr:has(td)'),
+			// Temporary delimiter characters unlikely to be typed by keyboard
+			// This is to avoid accidentally splitting the actual contents
+			tmpColDelim = String.fromCharCode(11), // vertical tab character
+			tmpRowDelim = String.fromCharCode(0), // null character
 
-            // Temporary delimiter characters unlikely to be typed by keyboard
-            // This is to avoid accidentally splitting the actual contents
-            tmpColDelim = String.fromCharCode(11), // vertical tab character
-            tmpRowDelim = String.fromCharCode(0), // null character
+			// actual delimiter characters for CSV format
+			colDelim = '","',
+			rowDelim = '"\r\n"',
 
-            // actual delimiter characters for CSV format
-            colDelim = '","',
-            rowDelim = '"\r\n"',
+			// Grab text from table into CSV formatted string
+			csv = '"' + $rows.map(function (i, row) {
+					var $row = $(row),
+							$cols = $row.find('td');
 
-            // Grab text from table into CSV formatted string
-            csv = '"' + $rows.map(function (i, row) {
-                var $row = $(row),
-                    $cols = $row.find('td');
+					return $cols.map(function (j, col) {
+							var $col = $(col),
+									text = $col.text();
 
-                return $cols.map(function (j, col) {
-                    var $col = $(col),
-                        text = $col.text();
+							return text.replace('"', '""'); // escape double quotes
 
-                    return text.replace('"', '""'); // escape double quotes
+					}).get().join(tmpColDelim);
 
-                }).get().join(tmpColDelim);
+			}).get().join(tmpRowDelim)
+					.split(tmpRowDelim).join(rowDelim)
+					.split(tmpColDelim).join(colDelim) + '"',
 
-            }).get().join(tmpRowDelim)
-                .split(tmpRowDelim).join(rowDelim)
-                .split(tmpColDelim).join(colDelim) + '"',
-
-            // Data URI
-            csvData = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
+			// Data URI
+			csvData = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
 
         $(this)
             .attr({
